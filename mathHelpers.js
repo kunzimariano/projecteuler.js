49cd80d3a408b2ce4378fbb64d8508e72f4045a9
kunzimariano <kunzi.mariano@gmail.com>
2015-10-22 15:47:25 -0300

    Changed mathHelpers so we can export the module properly.

--- mathHelpers.js
+++ mathHelpers.js
@@ -2,14 +2,15 @@
 
 let R = require('ramda');
 
-let isFactor = (candidate, n) => {
+exports.isFactor = (candidate, n) => {
   return n % candidate === 0;
 };
 
-let isEven = n => n % 2 === 0;
-let isOdd = n => n % 2 !== 0;
+exports.isEven = n => n % 2 === 0;
 
-let isPrime = n => {
+exports.isOdd = n => n % 2 !== 0;
+
+exports.isPrime = n => {
   if (n < 2) return false;
   return !R.pipe(
     R.unfold(x => x > Math.sqrt(n) ? false : [x, x + 1], 2),
