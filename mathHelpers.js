d06f8e84cd0ec7ad9394d47d16d43c1ef4967650
kunzimariano <kunzi.mariano@gmail.com>
2015-10-22 17:01:44 -0300

    Added sequence function. Fixed isPrime.

--- mathHelpers.js
+++ mathHelpers.js
@@ -10,10 +10,17 @@ exports.isEven = n => n % 2 === 0;
 
 exports.isOdd = n => n % 2 !== 0;
 
+exports.sequence = (first, last) => {
+  return R.unfold(n => n > last ? false : [n, n + 1], first);
+};
+
 exports.isPrime = n => {
   if (n < 2) return false;
-  return !R.pipe(
-    R.unfold(x => x > Math.sqrt(n) ? false : [x, x + 1], 2),
-    R.any(x => n % x === 0 || n % (x + 2) === 0)
+
+  let divisors = exports.sequence(2, Math.sqrt(n));
+
+  return !R.any(
+    x => n % x === 0 || n % (x + 2) === 0,
+    divisors
   );
 };
